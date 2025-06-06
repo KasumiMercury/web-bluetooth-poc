b32fea49d5211caf24562450afddfb10a0f6349f
KasumiMercury <naoki_shira@outlook.jp>
2025-06-06 14:47:46 +0900

    company filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import './App.css'
+import {useState} from "react";
 
 function App() {
+  const [companyIdInputState, setCompanyIdInputState] = useState<string>("0xffff");
 
   const BluetoothEnable = async () => {
+    let companyIdInput = companyIdInputState;
+    if (!companyIdInput.startsWith("0x")) {
+        companyIdInput = `0x${companyIdInput}`;
+    }
+
+    const companyId = Number.parseInt(companyIdInput, 16);
+    console.log(companyId);
+
     try {
       const device = await navigator.bluetooth.requestDevice({
         // acceptAllDevices: true,
         filters: [
           {
-            namePrefix: "Pixel"
+            // namePrefix: "Pixel"
+            manufacturerData: [{
+              companyIdentifier: companyId,
+            }]
           }
         ]
       })
@@ -19,11 +32,19 @@ function App() {
   }
 
   return (
-      <>
+      <div id={"panel"}>
+        <div id={"company-id-field"}>
+          <label htmlFor={"company-id"}>Company ID</label>
+          <input type={"text"} id={"company-id"} placeholder={"Company ID"} pattern={"(0x)?[0-9a-fA-F]{1,4}"}
+                 value={companyIdInputState}
+                 onChange={(e) => setCompanyIdInputState(e.target.value)}
+          />
+
+        </div>
         <button type={"button"} onClick={BluetoothEnable}>
           BL-enable
         </button>
-      </>
+      </div>
   )
 }
 
