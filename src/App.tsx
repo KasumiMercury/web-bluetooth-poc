b9929452d96c65c3f103ae2fe7f2701475560db3
KasumiMercury <naoki_shira@outlook.jp>
2025-06-06 14:38:37 +0900

    company filter
    diff --git a/src/App.css b/src/App.css
    index b9d355d..31fe92b 100644
    --- a/src/App.css
    +++ b/src/App.css
    @@ -1,42 +1,67 @@
     #root {
    -  max-width: 1280px;
    -  margin: 0 auto;
    -  padding: 2rem;
    -  text-align: center;
    +    max-width: 1280px;
    +    margin: 0 auto;
    +    padding: 2rem;
    +    text-align: center;
     }
    
     .logo {
    -  height: 6em;
    -  padding: 1.5em;
    -  will-change: filter;
    -  transition: filter 300ms;
    +    height: 6em;
    +    padding: 1.5em;
    +    will-change: filter;
    +    transition: filter 300ms;
     }
    +
     .logo:hover {
    -  filter: drop-shadow(0 0 2em #646cffaa);
    +    filter: drop-shadow(0 0 2em #646cffaa);
     }
    +
     .logo.react:hover {
    -  filter: drop-shadow(0 0 2em #61dafbaa);
    +    filter: drop-shadow(0 0 2em #61dafbaa);
     }
    
     @keyframes logo-spin {
    -  from {
    -    transform: rotate(0deg);
    -  }
    -  to {
    -    transform: rotate(360deg);
    -  }
    +    from {
    +        transform: rotate(0deg);
    +    }
    +    to {
    +        transform: rotate(360deg);
    +    }
     }
    
     @media (prefers-reduced-motion: no-preference) {
    -  a:nth-of-type(2) .logo {
    -    animation: logo-spin infinite 20s linear;
    -  }
    +    a:nth-of-type(2) .logo {
    +        animation: logo-spin infinite 20s linear;
    +    }
     }
    
     .card {
    -  padding: 2em;
    +    padding: 2em;
     }
    
     .read-the-docs {
    -  color: #888;
    +    color: #888;
    +}
    +
    +#panel {
    +    display: flex;
    +    flex-direction: column;
    +    align-items: center;
    +    gap: 1rem;
    +}
    +
    +#company-id-field {
    +    display: flex;
    +    flex-direction: row;
    +    align-items: center;
    +    gap: 0.5rem;
    +}
    +
    +#company-id:invalid {
    +    border-color: red;
    +}
    +
    +.error {
    +    color: red;
    +    font-weight: bold;
     }
    diff --git a/src/App.tsx b/src/App.tsx
    index 395da25..817a946 100644
    --- a/src/App.tsx
    +++ b/src/App.tsx
    @@ -5,28 +5,14 @@ function App() {
       const BluetoothEnable = async () => {
         try {
           const device = await navigator.bluetooth.requestDevice({
    -        acceptAllDevices: true,
    -        optionalServices: ['2c081c6d-61dd-4af8-ac2f-17f2ea5e5214']
    -        // filters: [
    -        //   {
    -        //     namePrefix: "Pixel"
    -        //   }
    -        // ]
    +        // acceptAllDevices: true,
    +        filters: [
    +          {
    +            namePrefix: "Pixel"
    +          }
    +        ]
           })
           console.log("selected device:", device);
    -
    -      const server = await device.gatt?.connect();
    -      console.log("connected to server:", server);
    -
    -      const services = await server?.getPrimaryServices();
    -      console.log("services:", services);
    -
    -      if (services) {
    -        for (const service of services) {
    -          console.log("service:", service);
    -        }
    -      }
    -
         } catch (error) {
           console.log(error);
         }

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,14 @@ function App() {
   const BluetoothEnable = async () => {
     try {
       const device = await navigator.bluetooth.requestDevice({
-        acceptAllDevices: true,
-        optionalServices: ['2c081c6d-61dd-4af8-ac2f-17f2ea5e5214']
-        // filters: [
-        //   {
-        //     namePrefix: "Pixel"
-        //   }
-        // ]
+        // acceptAllDevices: true,
+        filters: [
+          {
+            namePrefix: "Pixel"
+          }
+        ]
       })
       console.log("selected device:", device);
-
-      const server = await device.gatt?.connect();
-      console.log("connected to server:", server);
-
-      const services = await server?.getPrimaryServices();
-      console.log("services:", services);
-
-      if (services) {
-        for (const service of services) {
-          console.log("service:", service);
-        }
-      }
-
     } catch (error) {
       console.log(error);
     }
